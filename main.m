fc1e96816f6312c517856d37683787d22cc15e83
grwei <313017602@qq.com>
2020-03-15 15:37:52 +0800

    create main.m
    write codes here.

---
+++ main.m
@@ -0,0 +1,9 @@
+%% Basic Information
+% Overview
+% Transmission-line parameters extractor
+% MATLAB implementation of Patent US8892414B1
+% Author Name: Guorui Wei
+% Instructor Name: Bin Xia
+% Created in: 2020-03-15 12:45
+
+clc; clear; close all;
