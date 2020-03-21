f24d6b9dd6a594ea2aa0bd2e80938093f69a42ce
Guorui Wei <44464432+grwei@users.noreply.github.com>
2020-03-21 09:36:55 +0800

    Update `test2.m`
    rewrite data-visualization logic

--- test2.m
+++ test2.m
@@ -97,14 +97,15 @@ for freqIdx = 1:freqPts
     rlgc_PowerSI.C(3,2,freqIdx) = rlgc_PowerSI.C(2,3,freqIdx);
     rlgc_PowerSI.C(4,1,freqIdx) = rlgc_PowerSI.C(1,4,freqIdx);
     rlgc_PowerSI.C(4,2,freqIdx) = rlgc_PowerSI.C(2,4,freqIdx);
-    rlgc_PowerSI.C(4,3,freqIdx) = rlgc_PowerSI.C(3,4,freqIdx);    
+    rlgc_PowerSI.C(4,3,freqIdx) = rlgc_PowerSI.C(3,4,freqIdx);
 end
 
 %% Extract RLGC params using proposed method
 
 rlgc_t = s2rlgc_t(SingleEnded8PortData.S_Parameters,lineLength,freq,z0);
 
-%% 3. Extract RLGC params using proposed method
+%% 
+
 figure('Name','Extracted RLGC using proposed method(before 1st resonance frequency)')
 sgtitle({'Extracted RLGC using proposed algorithm','(before 1st resonance frequency)'})
 subplot(2,2,1)
@@ -248,110 +249,74 @@ title('G matrix')
 
 %% Extracted RLGC compared with Cadence PowerSI
 
-% R, L
-figure('Name','R, L matrix')
-sgtitle({'Comparison Between Proposed Algorithm and';' Cadence Sigrity PowerSI: R, L Matrix'})
-subplot(221)
-plot(freq/1e9,squeeze(rlgc_t.R(1,1,:)),'k-')
-hold on
-plot(freq/1e9,squeeze(rlgc_PowerSI.R(1,1,:)),'g--')
-hold off
-grid on
-xlabel('Freq(GHz)');
-ylabel('R11(Ohms/m)');
-title('R11 Comparison');
-legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
-legend('boxoff')
+% R
+figure('Name','R matrix')
+sgtitle({'Comparison Between Proposed Algorithm and';' Cadence Sigrity PowerSI: R Matrix'})
+total = ceil(numOfLines/2);
+for idx = 1:numOfLines
+    subplot(2,total,idx)
+    plot(freq/1e9,squeeze(rlgc_t.R(1,idx,:)),'k-')
+    hold on
+    plot(freq/1e9,squeeze(rlgc_PowerSI.R(1,idx,:)),'g--')
+    hold off
+    grid on
+    xlabel('Freq(GHz)');
+    ylabel([sprintf('R1%u(Ohms/m)',idx)]);
+    title([sprintf('R1%u',idx)]);
+    legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
+    legend('boxoff')
+end
 
-subplot(222)
-plot(freq/1e9,squeeze(rlgc_t.R(1,2,:)),'k-')
-hold on
-plot(freq/1e9,squeeze(rlgc_PowerSI.R(1,2,:)),'g--')
-hold off
-grid on
-xlabel('Freq(GHz)');
-ylabel('R12(Ohms/m)');
-title('R12 Comparison');
-legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
-legend('boxoff')
+% L
+figure('Name','L matrix')
+sgtitle({'Comparison Between Proposed Algorithm and';' Cadence Sigrity PowerSI: L Matrix'})
+total = ceil(numOfLines/2);
+for idx = 1:numOfLines
+    subplot(2,total,idx)
+    plot(freq/1e9,squeeze(rlgc_t.L(1,idx,:)),'k-')
+    hold on
+    plot(freq/1e9,squeeze(rlgc_PowerSI.L(1,idx,:)),'g--')
+    hold off
+    grid on
+    xlabel('Freq(GHz)');
+    ylabel([sprintf('L1%u(H/m)',idx)]);
+    title([sprintf('L1%u',idx)]);
+    legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
+    legend('boxoff')
+end
 
-subplot(223)
-plot(freq/1e9,squeeze(rlgc_t.L(1,1,:)),'k-')
-hold on
-plot(freq/1e9,squeeze(rlgc_PowerSI.L(1,1,:)),'g--')
-hold off
-grid on
-xlabel('Freq(GHz)');
-ylabel('L11(H/m)');
-title('L11 Comparison');
-legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
-legend('boxoff')
+% G
+figure('Name','G matrix')
+sgtitle({'Comparison Between Proposed Algorithm and';' Cadence Sigrity PowerSI: G Matrix'})
+total = ceil(numOfLines/2);
+for idx = 1:numOfLines
+    subplot(2,total,idx)
+    plot(freq/1e9,squeeze(rlgc_t.G(1,idx,:)),'k-')
+    hold on
+    plot(freq/1e9,squeeze(rlgc_PowerSI.G(1,idx,:)),'g--')
+    hold off
+    grid on
+    xlabel('Freq(GHz)');
+    ylabel([sprintf('G1%u(S/m)',idx)]);
+    title([sprintf('G1%u',idx)]);
+    legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
+    legend('boxoff')
+end
 
-subplot(224)
-plot(freq/1e9,squeeze(rlgc_t.L(1,2,:)),'k-')
-hold on
-plot(freq/1e9,squeeze(rlgc_PowerSI.L(1,2,:)),'g--')
-hold off
-grid on
-xlabel('Freq(GHz)');
-ylabel('L12(H/m)');
-title('L12 Comparison');
-legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
-legend('boxoff')
-
-% C, G
-figure('Name','C, G matrix')
-sgtitle({'Comparison Between Proposed Algorithm and';' Cadence Sigrity PowerSI: C, G Matrix'})
-subplot(221)
-plot(freq/1e9,squeeze(rlgc_t.C(1,1,:)),'m-')
-hold on
-plot(freq/1e9,squeeze(rlgc_PowerSI.C(1,1,:)),'k--')
-hold off
-grid on
-% ave_y = mean(squeeze(rlgc_t.C(1,1,:)));
-% del_r = 4e-3;
-% ylim([ave_y*(1-del_r), ave_y*(1+del_r)])
-xlabel('Freq(GHz)');
-ylabel('C11(F/m)');
-title('C11 Comparison');
-legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
-legend('boxoff')
-
-subplot(222)
-plot(freq/1e9,squeeze(rlgc_t.C(1,2,:)),'m-')
-hold on
-plot(freq/1e9,squeeze(rlgc_PowerSI.C(1,2,:)),'k--')
-hold off
-grid on
-% ave_y = mean(squeeze(rlgc_t.C(1,2,:)));
-% del_r = 5e-4;
-% ylim([ave_y*(1+del_r), ave_y*(1-del_r)])
-xlabel('Freq(GHz)');
-ylabel('C12(F/m)');
-title('C12 Comparison');
-legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
-legend('boxoff')
-
-subplot(223)
-plot(freq/1e9,squeeze(rlgc_t.G(1,1,:)),'m-')
-hold on
-plot(freq/1e9,squeeze(rlgc_PowerSI.G(1,1,:)),'k--')
-hold off
-grid on
-xlabel('Freq(GHz)');
-ylabel('G11(S/m)');
-title('G11 Comparison');
-legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
-legend('boxoff')
-
-subplot(224)
-plot(freq/1e9,squeeze(rlgc_t.G(1,2,:)),'m-')
-hold on
-plot(freq/1e9,squeeze(rlgc_PowerSI.G(1,2,:)),'k--')
-hold off
-grid on
-xlabel('Freq(GHz)');
-ylabel('G12(S/m)');
-title('G12 Comparison');
-legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
-legend('boxoff')
+% C
+figure('Name','C matrix')
+sgtitle({'Comparison Between Proposed Algorithm and';' Cadence Sigrity PowerSI: C Matrix'})
+total = ceil(numOfLines/2);
+for idx = 1:numOfLines
+    subplot(2,total,idx)
+    plot(freq/1e9,squeeze(rlgc_t.C(1,idx,:)),'k-')
+    hold on
+    plot(freq/1e9,squeeze(rlgc_PowerSI.C(1,idx,:)),'g--')
+    hold off
+    grid on
+    xlabel('Freq(GHz)');
+    ylabel([sprintf('C1%u(F/m)',idx)]);
+    title([sprintf('C1%u',idx)]);
+    legend({'Proposed Algorithm','Cadence Sigrity PowerSI'},'Location','best','NumColumns',1)
+    legend('boxoff')
+end
